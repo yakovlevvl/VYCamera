e42c6e9a9f456bcb56758d81de2f58a126cbab00
Vladyslav Yakovlev <yakovlev15@icloud.com>
2018-11-08 02:15:19 +0200

    Initial Commit

---
+++ VYCamera/VYCamera.h
@@ -0,0 +1,19 @@
+//
+//  VYCamera.h
+//  VYCamera
+//
+//  Created by Vladyslav Yakovlev on 11/8/18.
+//  Copyright © 2018 Vladyslav Yakovlev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VYCamera.
+FOUNDATION_EXPORT double VYCameraVersionNumber;
+
+//! Project version string for VYCamera.
+FOUNDATION_EXPORT const unsigned char VYCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VYCamera/PublicHeader.h>
+
+
